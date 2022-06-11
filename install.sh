9f2450a3d182557b885f1f035a3c9f6adb7b5c5d
senalbulumulle <senal.bulumulle@gmail.com>
2022-06-11 18:10:42 -0400

    Thanks and Credits to Chaotic-AUR

--- install.sh
+++ install.sh
@@ -49,6 +49,19 @@ while :
 
 	case $CHOICE in  
 
+		########################################################################
+		##	Credits to Chaotic-AUR
+		##
+		##	https://aur.chaotic.cx/
+		##	
+		########################################################################
+
+		pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com
+		pacman-key --lsign-key FBA220DFC880C036
+		pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+		cp pacman.conf /etc/pacman.conf		
+
 		#########################################################
 		## After the user chooses yes, then it will install the 
 		## dependencies and components for running Smith-Pad-OS
