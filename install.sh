273bacc66bb5d77a5a8b826b1d20cec35419bd5b
senalbulumulle <senal.bulumulle@gmail.com>
2022-06-11 18:50:21 -0400

    mmmm....

--- install.sh
+++ install.sh
@@ -49,18 +49,7 @@ while :
 
 	case $CHOICE in  
 
-		########################################################################
-		##	Credits to Chaotic-AUR
-		##
-		##	https://aur.chaotic.cx/
-		##	
-		########################################################################
-
-		pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com                                                                                         && \
-		pacman-key --lsign-key FBA220DFC880C036                                                                                                                         && \        
-		pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
-
-		cp pacman.conf /etc/pacman.conf		
+	
 
 		#########################################################
 		## After the user chooses yes, then it will install the 
@@ -70,6 +59,20 @@ while :
 
 		
 		yes)
+
+
+			########################################################################
+			##	Credits to Chaotic-AUR
+			##
+			##	https://aur.chaotic.cx/
+			##	
+			########################################################################
+
+			pacman-key --recv-key FBA220DFC880C036 --keyserver keyserver.ubuntu.com                                                                                         && \
+			pacman-key --lsign-key FBA220DFC880C036                                                                                                                         && \        
+			pacman -U 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-keyring.pkg.tar.zst' 'https://cdn-mirror.chaotic.cx/chaotic-aur/chaotic-mirrorlist.pkg.tar.zst'
+
+			cp pacman.conf /etc/pacman.conf	
 
 			#########################################################
                         ## 	Make sure the packages is up to date
