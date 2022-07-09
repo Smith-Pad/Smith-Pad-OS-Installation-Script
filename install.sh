4b5e65f0176345a945133457416bf6abf65cf451
senalbulumulle <senal.bulumulle@gmail.com>
2022-07-09 15:18:38 -0400

    I have a light bulb....

--- install.sh
+++ install.sh
@@ -9,20 +9,6 @@
 
 
 function introduction() {
-        dialog --title "Smith-Pad-OS Installation Script" --msgbox "This install script will install the Smith-Pad-OS components and dependencies on various Arch Linux-based distributions." 10 50
+
 
 }
-
-function requiredPackages() {
-	## Install packages required to run the Smith-Pad-OS-Installation-Script 
-	## first before proceeding to the next steps
-	sudo pacman -S --no-confirm dialog
-}
-
-function confirmation() {
-        
-}
-
-introduction
-requiredPackages
-confirmation
