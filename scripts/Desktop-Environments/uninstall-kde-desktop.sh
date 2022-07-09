680628f3ebadff027b252f29dd4ec4d3320f40c7
senalbulumulle <senal.bulumulle@gmail.com>
2022-07-09 19:33:22 -0400

    I have a light bulb part 4..

---
+++ scripts/Desktop-Environments/uninstall-kde-desktop.sh
@@ -0,0 +1,68 @@
+#####################################################################################
+##			uninstall-kde-desktop.sh
+##
+##
+##
+##		Uninstalls the KDE-Desktop environment with other apps and 
+##		dependencies.
+##
+##		
+#####################################################################################
+
+
+function dependencies() {
+	pacman -Rcns --noconfirm xdg-desktop-portal-kde
+	pacman -Rcns --noconfirm kde-cli-tools
+	pacman -Rcns --noconfirm kde-gtk-config
+	pacman -Rcns --noconfirm kgamma5
+	pacman -Rcns --noconfirm plasma-firewall
+	pacman -Rcns --noconfirm kwin
+	pacman -Rcns --noconfirm khotkeys
+	pacman -Rcns --noconfirm kmenuedit
+	pacman -Rcns --noconfirm kscreen
+	pacman -Rcns --noconfirm kscreenlocker
+	pacman -Rcns --noconfirm libkscreen
+	pacman -Rcns --noconfirm plasma-integration
+}
+
+
+
+function productivity() {
+	pacman -Rcns --noconfirm dolphin
+	pacman -Rcns --noconfirm kalendar
+	pacman -Rcns --noconfirm marktext
+}
+
+function development() {
+	pacman -Rcns --noconfirm kate
+	pacman -Rcns --noconfirm vscodium
+	pacman -Rcns --noconfirm nodejs
+	pacman -Rcns --noconfirm npm
+
+}
+
+
+function webBrowser() {
+	pacman -Rcns --noconfirm google-chrome
+	pacman -Rcns --noconfirm librewolf
+}
+
+
+function systemTools() {
+	pacman -Rcns --noconfirm konsole
+	pacman -Rcns --noconfirm gnome-disk-utility
+	pacman -Rcns --noconfirm gnome-font-viewer
+	pacman -Rcns --noconfirm gnome-control-center
+	pacman -Rcns --noconfirm gnome-remote-desktop
+	pacman -Rcns --noconfirm gnome-logs
+	pacman -Rcns --noconfirm gnome-Rcnsoftware
+	pacman -Rcns --noconfirm gnome-Rcnsystem-monitor
+	pacman -Rcns --noconfirm gnome-font-viewer
+	pacman -Rcns --noconfirm gnome-characters
+}
+
+dependencies
+productivity
+development
+Web Browser
+systemTools
