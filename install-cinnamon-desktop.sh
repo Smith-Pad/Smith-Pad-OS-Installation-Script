e4b84e5ef70f9cf4cd90ff09b4e7fa5cb56e8a2e
senalbulumulle <senal.bulumulle@gmail.com>
2022-07-17 15:13:38 -0400

    I have a light bulb part 18

---
+++ install-cinnamon-desktop.sh
@@ -0,0 +1,108 @@
+#####################################################################################
+##			install-cinnamon-desktop.sh
+##
+##
+##
+##		Installs the Cinnamon-Desktop environment with other apps and 
+##		dependencies.
+##
+##		
+#####################################################################################
+
+function dependencies() {
+        pacman -S --noconfirm caribou
+        pacman -S --noconfirm gnome-backgrounds
+        pacman -S --noconfirm libcroco
+        pacman -S --noconfirm libkeybinder3
+        pacman -S --noconfirm muffin
+        pacman -S --noconfirm nemo
+        pacman -S --noconfirm python-atspi
+        pacman -S --noconfirm python-pam
+        pacman -S --noconfirm python-pexpect
+        pacman -S --noconfirm python-ptyprocess
+        pacman -S --noconfirm python-pyinotify
+        pacman -S --noconfirm python-pytz
+        pacman -S --noconfirm python-setproctitle
+        pacman -S --noconfirm python-tinycss2
+        pacman -S --noconfirm python-python-webencodings
+        pacman -S --noconfirm python-python-xapp
+        pacman -S --noconfirm python-timezonemap
+        pacman -S --noconfirm python
+        pacman -S --noconfirm cinnamon-settings-daemon
+        pacman -S --noconfirm cinnamon-menus
+        pacman -S --noconfirm xapp
+        pacman -S --noconfirm xorg
+}
+
+
+
+function productivity() {
+        pacman -S --noconfirm nemo
+        pacman -S --noconfirm marktext
+        pacman -S --noconfirm libreoffice
+}
+
+function development() {
+        pacman -S --noconfirm gedit
+        pacman -S --noconfirm vscodium
+        pacman -S --noconfirm gnome-builder
+
+}
+
+
+function webBrowser() {
+        pacman -S --noconfirm google-chrome
+        pacman -S --noconfirm librewolf
+}
+
+
+function systemTools() {
+        pacman -S --noconfirm nodejs
+        pacman -S --noconfirm npm
+
+}
+
+
+function enableServices() {
+        echo "later on..."
+}
+
+
+function repeatMenu() {
+        clear
+	echo """
+	########################################################################################
+	##      Welcome to the Smith-Pad-OS-Installer.                                         #
+	########################################################################################
+
+	########################################################################################
+	##      WARNING: This script may be unstable at the moment. Please make sure you       #
+	##               backup your system before running this script.                        #
+	########################################################################################
+
+	########################################################################################
+	##      Options                                                                        #
+	##                                                                                     #
+	########################################################################################
+
+	########################################################################################
+	##      To get the options, please type 'options'. 
+	########################################################################################
+
+
+	########################################################################################
+	##	Exiting the program
+	##
+	##
+	##	Type 'exit' to exit the program
+	########################################################################################
+        """                            
+}
+
+ dependencies
+ productivity
+ development
+ webBrowser
+ systemTools
+ enableServices
+ repeatMenu
---
+++ uninstall-cinnamon-desktop.sh
@@ -0,0 +1,113 @@
+#####################################################################################
+##			uninstall-cinnamon-desktop.sh
+##
+##
+##
+##		Installs the Cinnamon-Desktop environment with other apps and 
+##		dependencies.
+##
+##		
+#####################################################################################
+
+function disableServices() {
+        echo "later on..."
+}
+
+function dependencies() {
+        pacman -Rcns --noconfirm caribou
+        pacman -Rcns --noconfirm gnome-backgrounds
+        pacman -Rcns --noconfirm libcroco
+        pacman -Rcns --noconfirm libkeybinder3
+        pacman -Rcns --noconfirm muffin
+        pacman -Rcns --noconfirm nemo
+        pacman -Rcns --noconfirm python-atspi
+        pacman -Rcns --noconfirm python-pam
+        pacman -Rcns --noconfirm python-pexpect
+        pacman -Rcns --noconfirm python-ptyprocess
+        pacman -Rcns --noconfirm python-pyinotify
+        pacman -Rcns --noconfirm python-pytz
+        pacman -Rcns --noconfirm python-setproctitle
+        pacman -Rcns --noconfirm python-tinycss2
+        pacman -Rcns --noconfirm python-python-webencodings
+        pacman -Rcns --noconfirm python-python-xapp
+        pacman -Rcns --noconfirm python-timezonemap
+        pacman -Rcns --noconfirm python
+        pacman -Rcns --noconfirm cinnamon-settings-daemon
+        pacman -Rcns --noconfirm cinnamon-menus
+        pacman -Rcns --noconfirm xapp
+        pacman -Rcns --noconfirm xorg
+}
+
+
+
+function productivity() {
+        pacman -Rcns --noconfirm nemo
+        pacman -Rcns --noconfirm marktext
+        pacman -Rcns --noconfirm libreoffice
+}
+
+function development() {
+        pacman -Rcns --noconfirm gedit
+        pacman -Rcns --noconfirm vscodium
+        pacman -Rcns --noconfirm gnome-builder
+
+}
+
+
+function webBrowser() {
+        pacman -Rcns --noconfirm google-chrome
+        pacman -Rcns --noconfirm librewolf
+}
+
+
+function systemTools() {
+        pacman -Rcns --noconfirm nodejs
+        pacman -Rcns --noconfirm npm
+
+}
+
+
+function enableServices() {
+        systemctl enable gdm
+        systemctl start gdm
+}
+
+
+function repeatMenu() {
+        clear
+	echo """
+	########################################################################################
+	##      Welcome to the Smith-Pad-OS-Installer.                                         #
+	########################################################################################
+
+	########################################################################################
+	##      WARNING: This script may be unstable at the moment. Please make sure you       #
+	##               backup your system before running this script.                        #
+	########################################################################################
+
+	########################################################################################
+	##      Options                                                                        #
+	##                                                                                     #
+	########################################################################################
+
+	########################################################################################
+	##      To get the options, please type 'options'. 
+	########################################################################################
+
+
+	########################################################################################
+	##	Exiting the program
+	##
+	##
+	##	Type 'exit' to exit the program
+	########################################################################################
+        """                            
+}
+
+ dependencies
+ productivity
+ development
+ webBrowser
+ systemTools
+ disableServices
+ repeatMenu
